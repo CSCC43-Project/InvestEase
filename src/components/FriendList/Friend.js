ef98c73e66d6c4ea12223350f94e70128a91c512
cassy18 <113991059+cassy18@users.noreply.github.com>
2024-07-18 17:44:46 -0400

    Edited Friend List in User Profile

--- src/components/FriendList/Friend.js
+++ src/components/FriendList/Friend.js
@@ -1,9 +1,15 @@
 import "./Friend.css";
+import { useState } from "react";
 function Friend() {
+    const [friend, setFriend] = useState({
+        name: "John Doe",
+        username: "johndoe",
+        avatar: "https://randomuser.me/api/portraits/men/1.jpg",
+    });
     return (
         <div className="singlefriend">
-            <img className="profilepic" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png"></img>
-            <h1>Username</h1>
+            <img className="profilepic" src={friend.avatar}></img>
+            <h1 style={{ marginRight: "1rem" }}>{friend.username}</h1>
         </div>
     );
 }
--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,18 +1,36 @@
 import "./FriendList.css";
 import Friend from "./Friend";
+import { useState } from "react";
+
 function FriendList() {
+    const [friend, setFriend] = useState(true);
+    const [incoming, setIncoming] = useState(false);
+    const [outgoing, setOutgoing] = useState(false);
     return (
         <div className="profile">
             <div className="friendslist">
                 <div className="friendslist-header">
-                    <button className="friendbutton">Friends</button>
-                    <button className="friendbutton">Incoming</button>
-                    <button className="friendbutton">Outgoing</button>
+                    <button className="friendbutton" onClick={() => { setFriend(true); setIncoming(false); setOutgoing(false); }}>Friends</button>
+                    <button className="friendbutton" onClick={() => { setFriend(false); setIncoming(true); setOutgoing(false); }}>Incoming</button>
+                    <button className="friendbutton" onClick={() => { setFriend(false); setIncoming(false); setOutgoing(true); }}>Outgoing</button>
                 </div>
                 <div className="friends">
-                    <Friend />
-                    <Friend />
-                    <Friend />
+                    {friend && 
+                    <div>
+                        <Friend />
+                    </div>}
+                    {incoming &&
+                    <div>
+                        <Friend />
+                        <Friend />
+                    </div>}
+                    {outgoing && 
+                    <div>
+                        <Friend />
+                        <Friend />
+                        <Friend />
+                        <Friend />
+                    </div>}
                 </div>
             </div>
         </div>
