b2f1b1ed7017022588b7923288f545f442cfeb4e
cassy18 <113991059+cassy18@users.noreply.github.com>
2024-07-24 14:44:43 -0400

    commented necessary queries

--- src/backend/index.js
+++ src/backend/index.js
@@ -6,7 +6,43 @@ const pool = require('./db');
 app.use(cors());
 app.use(express.json());
 
-// get all users
+// ! REGISTER PAGE
+// INSERT INTO users (userid, email, username, password, profile_pic) VALUES (__, $1, $2, $3, $4) RETURNING *;
+
+// ! LOGIN PAGE
+// SELECT * FROM users WHERE username = $1 AND password = $2;
+
+// ! PROFILE PAGE
+// SELECT username FROM users WHERE userid = $1;
+// SELECT profile_pic FROM users WHERE userid = $1;
+    // ? FriendsList
+    // SELECT * FROM friends_list WHERE ownerID = $1;
+    // ? FriendRequest: incoming
+    // SELECT * FROM friend_requests WHERE receiverID = $1;
+    // ? FriendRequest: outgoing
+    // SELECT * FROM friend_requests WHERE senderID = $1;
+    // ? FriendRequest: send/receive
+    // INSERT INTO friend_requests (senderID, receiverID) VALUES ($1, $2);
+
+// ! USERS PORTFOLIOS
+    // ? Portfolio: Create new portfolio
+    // INSERT INTO portfolios (userid, portfolio_name, cash_account) VALUES ($1, $2, $3) RETURNING *;
+    // ? Portfolio: Get all portfolios
+    // SELECT * FROM portfolios WHERE userid = $1;
+    // ? Portfolio: Get specific portfolios
+    // SELECT * FROM portfolios WHERE userid = $1 AND portfolio_id = $2;
+    // SELECT * FROM stock_holding WHERE portfolio_id = $1 AND userid = $2 AND stock_symbol = $3 AND s;
+    // ? StockHolding: Get all Stock Holdings
+    // SELECT * FROM stock_holding WHERE portfolio_id = $1 AND userid = $2;
+    // ? StockHolding: Get specific Stock Holding
+    // SELECT * FROM stock_holding WHERE portfolio_id = $1 AND userid = $2 AND stock_symbol = $3 AND timestamp = $4;
+    // ? StockHolding: Add stock to portfolio
+    // INSERT INTO stock_holding (portfolio_id, userid, stock_symbol, timestamp, time_of_purchase, num_shares) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;
+    // * I'm not sure what else we need to do for the stock holding table
+
+
+
+    // get all users
 app.get('/users', async (req, res) => {
     try {
         const allUsers = await pool.query('SELECT * FROM users');
